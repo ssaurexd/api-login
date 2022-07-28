30badac0a8069894582b3f24f81daca32a43a1e8
Aure <ssaurexd@gmail.com>
2022-07-27 21:08:25 -0500

    agregado localhost a prod

--- src/config/server.ts
+++ src/config/server.ts
@@ -43,7 +43,7 @@ class Server {
 		if( process.env.NODE_ENV === 'production' ) {
 
 			whiteList = [
-				''	
+				'http://localhost:3000'	
 			]
 		} else whiteList = ['http://localhost:3000', 'http://localhost:4000']
 		this.app.use( cors({
